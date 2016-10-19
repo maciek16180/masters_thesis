4067197834ec48f7be4a86286c20bb1df9e0d3a1
Maciej Pawlikowski <i258346@lab110-02.stud.ii>
2016-10-19 17:52:10 +0200

    added training for rnn_ex

---
+++ set-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# The directory where the script is
+export NN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ \( "`which ipython`" != /pio/os/anaconda/bin/ipython \) -a \( -e /pio/os/anaconda/set-env.sh \) ]
+then
+    echo "Adding Anaconda Python from /pio/os to your environment"
+    source /pio/os/anaconda/set-env.sh
+fi
+
+export FUEL_DATA_PATH=/pio/data/data/fuel/
+
+export BLOCKS_CONFIG=$NN/config/blocks.yaml
+export THEANORC=$NN/config/theano.rc:$HOME/.theanorc
+
+#python modules
+export PYTHONPATH=$NN:$NN/libs/Theano:$NN/libs/fuel:$NN/libs/picklable-itertools:$PYTHONPATH
+export PATH=$NN/libs/fuel/bin:$PATH
