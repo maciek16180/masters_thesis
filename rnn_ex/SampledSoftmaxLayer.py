d63e5947fa0adaa436f224910db33647d8a369de
Maciej Pawlikowski <i258346@lab110-10.stud.ii>
2016-11-23 17:40:07 +0100

    sampled softmax now samples targets at random, loading dataset fixed, added partial support for fixed embeddings

--- rnn_ex/SampledSoftmaxLayer.py
+++ rnn_ex/SampledSoftmaxLayer.py
@@ -1,12 +1,16 @@
 import theano.tensor as T
+import numpy as np
 from lasagne import init
 from lasagne.layers import Layer,MergeLayer, InputLayer
 
 
 class SampledSoftmaxDenseLayer(MergeLayer):
-    # voc_mask is a mask vector of length voc_size
+    # samples is a mask vector of length voc_size
     
-    def __init__(self, incoming, voc_mask, voc_size, targets=None,
+    def __init__(self, incoming, num_sampled, voc_size, 
+                 targets=None, 
+                 use_all_words=False,
+                 sample_unique=True,
                  W_init = init.GlorotUniform(),
                  b_init = init.Constant(0),
                  **kwargs):
@@ -14,13 +18,6 @@ class SampledSoftmaxDenseLayer(MergeLayer):
         assert len(incoming.output_shape) == 2
         
         incomings = [incoming]
-        
-        if not isinstance(voc_mask, Layer):
-            assert voc_mask.ndim == 1
-            voc_mask_shape = (voc_size,)
-            voc_mask = InputLayer(voc_mask_shape, input_var=voc_mask, name="voc_mask inputlayer")
-            
-        incomings.append(voc_mask)
         
         if targets is not None:
             if not isinstance(targets, Layer):
@@ -33,6 +30,9 @@ class SampledSoftmaxDenseLayer(MergeLayer):
         super(SampledSoftmaxDenseLayer,self).__init__(incomings,**kwargs)
         
         self.voc_size = voc_size
+        self.num_sampled = num_sampled
+        self.use_all_words = use_all_words
+        self.sample_unique = sample_unique
         
         n_inputs = incoming.output_shape[1]
         self.W = self.add_param(W_init, (n_inputs, self.voc_size), name="W")
@@ -41,30 +41,53 @@ class SampledSoftmaxDenseLayer(MergeLayer):
     def get_output_for(self, inputs, **kwargs):
         #
         # if targets are NOT provided: 
-        #     returns softmax output calculated only on where voc_mask == 1,
+        #     returns softmax output calculated only on where samples == 1,
         #     output is surrounded by zeros to match the shape of a full softmax output
         # otherwise:
         #     returns sampled softmax output only for specified class for each sample
         
-        assert len(inputs) in [2,3]
-        input, voc_mask = inputs[:2]
+        assert len(inputs) in [1,2]
+        input = inputs[0]
         
-        idx = voc_mask.nonzero()[0]        
-        input = input.dot(self.W[:,idx]) + self.b[idx]
-        ssoft = T.nnet.softmax(input - T.log(1. / idx.shape[0])) # the -log is for normalization of sampled softmax
+        # if targets are provided, return only probs for targets
+        if len(inputs) == 2:
+            targets = inputs[1]
+            
+            if not self.use_all_words:
+                if self.sample_unique:
+                    samples = T.as_tensor(np.random.choice(np.arange(self.voc_size),
+                                                           size=self.num_sampled,
+                                                           replace=False))
+                else:
+                    samples = T.as_tensor(np.random.randint(self.voc_size, size=self.num_sampled))
+
+                true_logits = (input * self.W[:,targets].T).sum(axis=1) + self.b[targets]
+                sampled_logits = input.dot(self.W[:,samples]) + self.b[samples]
+                #
+                # here we should subtract log(Q(y|x)), but using uniform sampling with replacement
+                # makes all of the Q values equal, so it's unnecessary
+                #
+                logits = T.concatenate([true_logits.dimshuffle((0,'x')), sampled_logits], axis=1)
+                ssoft = T.nnet.softmax(logits)
+            
+                return ssoft[:,0] # only values for targets
+            
+            else: # this part is for validation, where we use full softmax loss
+                logits = input.dot(self.W) + self.b
+                soft = T.nnet.softmax(logits)
+            
+                return soft[T.arange(soft.shape[0]), targets]
         
-        if len(inputs) == 3:
-            targets = inputs[2]
-            target_idx_in_masked_voc = T.extra_ops.cumsum(voc_mask)[targets] - 1
-            return ssoft[T.arange(ssoft.shape[0]), target_idx_in_masked_voc]
-        
+        # if targets are not provided, return full softmax                
         else:
-            out = T.zeros((input.shape[0], self.voc_size))
-            return T.set_subtensor(out[:,idx], ssoft)
+            logits = input.dot(self.W) + self.b
+            soft = T.nnet.softmax(logits)
+            
+            return soft
         
     def get_output_shape_for(self, input_shapes, **kwargs):
-        assert len(input_shapes) in [2,3]
-        if len(input_shapes) == 3:
+        assert len(input_shapes) in [1,2]
+        if len(input_shapes) == 2:
             return (input_shapes[0][0],)
         else:
             return (input_shapes[0][0], self.voc_size)    
---
+++ rnn_ex/SampledSoftmaxLayer_OLD.py
@@ -0,0 +1,73 @@
+import theano.tensor as T
+from lasagne import init
+from lasagne.layers import Layer,MergeLayer, InputLayer
+
+
+class SampledSoftmaxDenseLayer(MergeLayer):
+    # voc_mask is a mask vector of length voc_size
+    
+    def __init__(self, incoming, voc_mask, voc_size, targets=None,
+                 W_init = init.GlorotUniform(),
+                 b_init = init.Constant(0),
+                 **kwargs):
+
+        assert len(incoming.output_shape) == 2
+        
+        incomings = [incoming]
+        
+        if not isinstance(voc_mask, Layer):
+            assert voc_mask.ndim == 1
+            voc_mask_shape = (voc_size,)
+            voc_mask = InputLayer(voc_mask_shape, input_var=voc_mask, name="voc_mask inputlayer")
+            
+        incomings.append(voc_mask)
+        
+        if targets is not None:
+            if not isinstance(targets, Layer):
+                assert targets.ndim == 1
+                targets_shape = (incoming.output_shape[0],)
+                targets = InputLayer(targets_shape, input_var=targets, name="targets inputlayer")
+
+            incomings.append(targets)
+        
+        super(SampledSoftmaxDenseLayer,self).__init__(incomings,**kwargs)
+        
+        self.voc_size = voc_size
+        
+        n_inputs = incoming.output_shape[1]
+        self.W = self.add_param(W_init, (n_inputs, self.voc_size), name="W")
+        self.b = self.add_param(b_init, (self.voc_size,), name="b", regularizable=False)
+        
+    def get_output_for(self, inputs, **kwargs):
+        #
+        # if targets are NOT provided: 
+        #     returns softmax output calculated only on where voc_mask == 1,
+        #     output is surrounded by zeros to match the shape of a full softmax output
+        # otherwise:
+        #     returns sampled softmax output only for specified class for each sample
+        
+        assert len(inputs) in [2,3]
+        input, voc_mask = inputs[:2]
+        
+        idx = voc_mask.nonzero()[0]        
+        input = input.dot(self.W[:,idx]) + self.b[idx]
+        ssoft = T.nnet.softmax(input - T.log(1. / idx.shape[0])) # the -log is for normalization of sampled softmax
+        
+        if len(inputs) == 3:
+            targets = inputs[2]
+            target_idx_in_masked_voc = T.extra_ops.cumsum(voc_mask)[targets] - 1
+            return ssoft[T.arange(ssoft.shape[0]), target_idx_in_masked_voc]
+        
+        else:
+            out = T.zeros((input.shape[0], self.voc_size))
+            return T.set_subtensor(out[:,idx], ssoft)
+        
+    def get_output_shape_for(self, input_shapes, **kwargs):
+        assert len(input_shapes) in [2,3]
+        if len(input_shapes) == 3:
+            return (input_shapes[0][0],)
+        else:
+            return (input_shapes[0][0], self.voc_size)    
+    
+    
+    
